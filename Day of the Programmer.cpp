62da1c996c860baf093a909fa1dce25aba5cd010
sam7834 <57521056+sam7834@users.noreply.github.com>
2020-05-23 20:04:13 +0530

    Create  Day of the Programmer.cpp

---
+++ Day of the Programmer.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+// Complete the dayOfProgrammer function below.
+string dayOfProgrammer(int year) {
+    string diff;
+    if(year< 1918){
+        if((year%4) ==0)
+            diff = "13";
+        else
+            diff = "12";
+    }
+    if(year> 1918){
+        if((year%4) ==0 && (year%400)==0 && (year%100)!=0)
+            diff = "13";
+        else
+            diff = "12";
+    }
+    return diff;
+}
+
+int main(){
+    string year;
+    getline(cin,year);
+    int day = dayOfProgrammer(year);
+    cout << day << ".09." << year;
+    return 0;
+}
