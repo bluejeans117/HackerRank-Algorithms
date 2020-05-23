3329869b3e0f82888b519b27bda9f699c87f6bf4
sam7834 <57521056+sam7834@users.noreply.github.com>
2020-05-23 23:08:30 +0530

    Update Mini-Max Sum.cpp

--- Mini-Max Sum.cpp
+++ Mini-Max Sum.cpp
@@ -2,10 +2,11 @@
 
 using namespace std;
 
-void fun(vector<int> nums){
+void fun(vector<long> nums){
     sort(nums.begin(), nums.end());
     int n =nums.size();
-    int i, min =0, max=0;
+    int i;
+    long min =0, max=0;
     for(i=0;i<n-1;i++){
         min+=nums[i];
     }
@@ -16,8 +17,9 @@ void fun(vector<int> nums){
 
 }
 int main(){
-    vector<int> nums;
-    int x;
+    vector<long> nums;
+    long x;
+    int i;
     for(i=0;i<5;i++){
         cin>>x;
         nums.push_back(x);
