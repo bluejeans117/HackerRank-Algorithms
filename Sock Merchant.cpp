4cae4434d2ffec35e0e883a755525b2683d75b62
sam7834 <57521056+sam7834@users.noreply.github.com>
2020-05-25 14:50:13 +0530

    Update Sock Merchant.cpp

--- Sock Merchant.cpp
+++ Sock Merchant.cpp
@@ -1,19 +1,21 @@
-
 #include <bits/stdc++.h>
 using namespace std;
 
 void fun(int n , vector<int> a){
     sort(a.begin(), a.end());
-    int i=0, c=1, p=0;
+    int i=0,m=0, c=1, p=0;
     //for(i=0;i<n;i++){
     while (i<n-1) {
         if(a[i]==a[i+1]){
             c+=1;
             i++;
+            //p+=c/2;
         }
         else if(a[i]!=a[i+1]){
-            p+=c/2;
-            c=0;
+            m=c/2;
+            p+=m;
+            c=1;
+            i++;
         }
     }
     cout << p ;       
