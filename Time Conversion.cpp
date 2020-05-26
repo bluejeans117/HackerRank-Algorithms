3780649cde74d581dc0c34ae35866b13edab46ee
sam7834 <57521056+sam7834@users.noreply.github.com>
2020-05-26 16:55:03 +0530

    Update Time Conversion.cpp

--- Time Conversion.cpp
+++ Time Conversion.cpp
@@ -8,29 +8,35 @@ using namespace std;
 string timeConversion(string s) {
     //string hr;
     if(s[8]=='P'){
-       if(s[0]=='1'&& s[1]=='2'){
+       if(s[0]=='1' && s[1]=='2'){ // && s[3]=='0' && s[4]=='0' && s[6]=='0' && s[7]=='0'){
             s.pop_back();
             s.pop_back();
         }
         else{
-        s[0] = s[0]+'1';
-        s[1] = s[1]+'2';     //add 12 to given hh
+        s[0] = s[0]+1;
+        s[1] = s[1]+2;     //add 12 to given hh
         s.pop_back();
         s.pop_back();
         }
     }
     else if(s[8]=='A'){
         //hr=s[0]+s[1];
-        if(s[0]=='1'&& s[1]=='2'){
+        if(s[0]=='1'&& s[1]=='2'){ //&& s[3]=='0' && s[4]=='0' && s[6]=='0' && s[7]=='0'){
             s[0]=0;
             s[1]=0;
-            s[3]=0;
-            s[4]=0;
-            s[6]=0;
-            s[7]=0;
+            
             s.pop_back();
             s.pop_back();
         }
+        /*else if(s[0]=='1'&& s[1]=='2'){
+            s[0]=0;
+            s[1]=0;*/
+            /*s[3]=0;
+            s[4]=0;
+            s[6]=0;
+            s[7]=0;*/
+
+        
         else{
             s.pop_back();
             s.pop_back();
