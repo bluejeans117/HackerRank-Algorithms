0ac36da7143c9fa293d99634fb8f803f0dda7742
sam7834 <57521056+sam7834@users.noreply.github.com>
2020-05-29 11:45:39 +0530

    Create Forming a Magic Square.cpp

---
+++ Forming a Magic Square.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the formingMagicSquare function below.
+int formingMagicSquare(vector<vector<int>> s) {
+    int n = s.size();
+    int i;
+    for(i=0;i<n;i++){
+        sum += accumulate(s.begin(), s.end());
+        c+= 15 - sum;
+    }
+
+    return sum ;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    vector<vector<int>> s(3);
+    for (int i = 0; i < 3; i++) {
+        s[i].resize(3);
+
+        for (int j = 0; j < 3; j++) {
+            cin >> s[i][j];
+        }
+
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    int result = formingMagicSquare(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
