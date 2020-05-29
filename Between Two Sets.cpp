0777b69855223225f8e1cbf923263224560221f8
sam7834 <57521056+sam7834@users.noreply.github.com>
2020-05-29 08:43:50 +0530

    Update Between Two Sets.cpp

--- Between Two Sets.cpp
+++ Between Two Sets.cpp
@@ -1,42 +1,34 @@
-
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int fun(int  m,int n,vector<int> a,vector<int> b ){
-    int i,j ;
-    sort(a.begin(), a.end());
-    sort(b.begin(), b.end()); 
-    
-    vector<int> z;
-    vector<int> y;
-    vector<int> f;
-
-
-    int max = a[m-1];
-    int min = b[0];
-    for(i=max;i<=(min/2);i++){          //check 1st set
+int getTotalX(vector<int> a, vector<int> b) {
+    int m = a.size();
+    int n = b.size();
+    int c=0, flag =0;
+    int i, j;
+    int s= b[0]/2;
+    for(i=a[m-1];i<s;i++){
         for(j=0;j<m;j++){
-            if(i%a[j] == 0){            //how to check i satisfies the condition for every element of a
-                z.push_back(i);         //array to store elts which satisfy the condtition
-                f[i]=1;
-            }
+            if(i % a[j] !=0)
+                flag =1;
         }
-
-        //for(j=0;j<n;j++){         //check 2nd set
-        while(j<n){
-            if(b[j]%z[i]==0)            //how to check i satisfies the condition for every element of a
-                y.push_back(z[i]);       //array to store elts which satisfy the condtition
+        for(j=0;j<n;j++){
+            if(b[j] % i !=0)
+                flag =1;
         }
-    }   
-    cout << y.size();
+        if(flag == 1)
+            c++;
+    }
+    return c;
 
 }
 
 int main(){
-    int x, i, m, n;
+    int m, n, i, x;
     vector<int> a;
-    vector<int> b ;
-    cin >>m>>n;
+    vector<int> b;
+
     for(i=0;i<m;i++){
         cin >> x;
         a.push_back(x);
@@ -45,5 +37,7 @@ int main(){
         cin >> x;
         b.push_back(x);
     }
-    fun(m,n,a,b)
+    int res = getTotalX(a, b );
+    cout <<res ;
+
 }
