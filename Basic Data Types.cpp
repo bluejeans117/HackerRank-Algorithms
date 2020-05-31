ecd820a6af3561502f9d6121aae2a5cc41bb569b
sam7834 <57521056+sam7834@users.noreply.github.com>
2020-05-31 11:56:51 +0530

    Create Basic Data Types.cpp

---
+++ Basic Data Types.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a ;
+    long b ;
+    char c ;
+    float d;
+    double e;
+    cin >> a >> b >> c>> d>> e;
+    cout << setprecision(20)<< a<< "\n"<< setprecision(20) << b <<"\n"<<setprecision(20) << c <<"\n"<<setprecision(20)<< d << "\n" <<setprecision(20)<< e;
+    return 0;
+}
