0d5167f009271b3874d8ae4867408ee1a168355f
Soukhya Mohan <57521056+sam7834@users.noreply.github.com>
2020-06-10 14:34:51 +0530

    Update Sparse Arrays.cpp

--- Sparse Arrays.cpp
+++ Sparse Arrays.cpp
@@ -1,26 +1,69 @@
-#include<bits/stdc++.h>
-using namespace std;
-/*
-    *
-    * Prosen Ghosh
-    * American International University - Bangladesh (AIUB)
-    *
-*/
-int main(){
+#include <bits/stdc++.h>
 
-    map<string,int>m;
-    int n,q;
-    string str;
-   
-    cin >> n;
-    for(int i = 0; i < n; i++){
-        cin >> str;
-        m[str]++;
+using namespace std;
+
+// Complete the matchingStrings function below.
+vector<int> matchingStrings(vector<string> strings, vector<string> queries) {
+    //vector<string, int> map;
+    unordered_map<string, int> map1;   //hope this works
+    int n = queries.size();
+    int m = strings.size();
+    vector<int> sam;
+    for(auto i:strings)
+        map1[i]++;
+
+    for(int i=0;i<n;i++){
+        string sam1= queries[i];
+        sam.push_back(map1[sam1]);
     }
-    cin >> q;
-    for(int i = 0; i < q; i++){
-        cin >> str;
-        cout << m[str] << endl;
+        
+  return sam;
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int strings_count;
+    cin >> strings_count;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    vector<string> strings(strings_count);
+
+    for (int i = 0; i < strings_count; i++) {
+        string strings_item;
+        getline(cin, strings_item);
+
+        strings[i] = strings_item;
     }
+
+    int queries_count;
+    cin >> queries_count;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    vector<string> queries(queries_count);
+
+    for (int i = 0; i < queries_count; i++) {
+        string queries_item;
+        getline(cin, queries_item);
+
+        queries[i] = queries_item;
+    }
+
+    vector<int> res = matchingStrings(strings, queries);
+
+    for (int i = 0; i < res.size(); i++) {
+        fout << res[i];
+
+        if (i != res.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
     return 0;
 }
