f939a181a58f90152c0962566dc4388d1b59050d
Soukhya Mohan <57521056+sam7834@users.noreply.github.com>
2020-06-11 01:04:12 +0530

    Create Print in Reverse.cpp

---
+++ Print in Reverse.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep) {
+    while (node) {
+        cout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            cout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the reversePrint function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+void reversePrint(SinglyLinkedListNode* head) {
+    vector<int> vect ;
+
+    SinglyLinkedListNode* temp = head;
+    while(head != NULL){
+        vect.push_back(head->data);
+        head= head->next;
+    }
+    reverse(vect.begin(), vect.end());
+    for(int i = 0; i<vect.size(); i++){
+        cout << vect[i] << endl;
+    }
+
+}
+
+int main()
